097289fabbba2e39f4e6090be1521d73b242ff0b
Dhruvin3103 <dcchawda2003@gmail.com>
2023-10-02 19:21:00 +0530

    added view for uploading csv to db

---
+++ backend/location/serializers.py
@@ -0,0 +1,7 @@
+from django.forms import FileField
+from rest_framework import serializers
+
+class LoadCSVSerializer(serializers.Serializer):
+    csv = serializers.FilePathField(path='csv/')
+    category = serializers.CharField()
+    
--- backend/location/views.py
+++ backend/location/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from .loadcsv import read_csv, import_csv_data
+from .serializers import LoadCSVSerializer
 # Create your views here.
-class LoadCSV(APIView):
-    
-    def get(self,request):
-        import_csv_data('csv/bus_stop_data.csv','bus_stop')
+class LoadCSV(GenericAPIView):
+    serializer_class = LoadCSVSerializer
+    def post(self,request):
+        print(request.data['csv'])
+        import_csv_data(request.data['csv'],request.data['category'])
         return Response({
             'message':'hello'
         })
